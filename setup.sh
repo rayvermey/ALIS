9125eb01d9e8949dbe4b7f11183d04be46018c8e
Ray Vermey <rayvermey@gmail.com>
2022-03-25 13:37:26 +0100

    setup.sh changed

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@ git clone https://github.com/arcolinux/arcolinux-spices
 git clone https://github.com/rayvermey/alis
 cd arcolinux-spices/usr/share/arcolinux-spices/scripts
 ./get-the-keys-and-repos.sh
-get-nemesis
-pacman -S arcolinux-system-config-git
+sudo pacman -Syy
+sudo pacman -S arcolinux-system-config-git
+sudo pacman -Syy
+get-nemesis-on-arcolinux
